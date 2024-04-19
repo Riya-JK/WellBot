caec9d65c3ade9096a91ea0080e12ad31645a5fa
Riya-JK <riyakannampuzha@gmail.com>
2024-04-18 22:31:08 -0400

    Handle invalid user input

--- chat_bot.py
+++ chat_bot.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split , cross_val_score
-from sklearn.tree import DecisionTreeClassifier, plot_tree , _tree
+from sklearn.tree import plot_tree , _tree
 from sklearn.ensemble import RandomForestClassifier
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -116,6 +116,9 @@ def main():
         combined_pattern = '|'.join(patterns)
         regexp = re.compile(combined_pattern)
         pred_list=[item for item in dis_list if regexp.search(item)]
+        if(len(pred_list) < 4):
+            print("Please enter a minimum of 4 symptoms for accurate diagnosis")
+            return -1,[]
         if(len(pred_list)>0):
             return 1,pred_list
         else:
@@ -153,7 +156,7 @@ def main():
 
         while True:
 
-            print("\nEnter the (3-4) main symptoms that you are experiencing for accurate consultation \t",end="->")
+            print("\nEnter the (3-4) main symptoms that you are experiencing for accurate diagnosis \t",end="->")
             disease_input = input("").lower()
             conf,cnf_dis=check_pattern(chk_dis,disease_input)
             if conf==1:
@@ -162,7 +165,7 @@ def main():
                     for num,it in enumerate(cnf_dis):
                         print(num,")",it)
                     if num!=0:
-                        print(f"Select the ones you meant (0 - {num}):  ", end="")
+                        print(f"Confirm the ones you meant (0 - {num}):  ", end="")
                         conf_input = input("").lower()
                         conf_inp = [int(x) for x in conf_input.split(',')]
                     else:
@@ -177,6 +180,8 @@ def main():
                     print("Oops! We don't have much information related to that symptom. Please visit a doctor near you for more information.")
                     return
                 break
+            elif conf == -1:
+                continue
             else:
                 print("Sorry we don't have much information on that symptom. Try a different symptom?")
 
@@ -235,7 +240,7 @@ def main():
                 print(second_prediction)
 
                 calc_condition(symptoms_exp,num_days)
-                if(present_disease[0]==second_prediction[0]):
+                if(len(present_disease) > 0 and len(second_prediction) > 0 and present_disease[0]==second_prediction[0]):
                     print("You may have ", present_disease[0])
                     print(description_list[present_disease[0]])
 
@@ -244,8 +249,10 @@ def main():
 
                 else:
                     print("You may have ", present_disease[0], "or ", second_prediction[0])
-                    print(description_list[present_disease[0]])
-                    print(description_list[second_prediction[0]])
+                    if len(description_list) > 0:
+                        print(description_list[present_disease[0]])
+                    if len(second_prediction) > 0:
+                        print(description_list[second_prediction[0]])
                 precution_list=precautionDictionary[present_disease[0]]
                 print("Take following measures : ")
                 for  i,j in enumerate(precution_list):
@@ -269,13 +276,17 @@ def main():
             ]
             symptoms_present = []
             returned_parent_nodes, confirm_symptoms_present = recurse(0,1, tree_, feature_name)
-            node_tree_symptom_mapping[(returned_parent_nodes[-1], tree_)] = confirm_symptoms_present
+            if(len(returned_parent_nodes) != 0):
+                node_tree_symptom_mapping[(returned_parent_nodes[-1], tree_)] = confirm_symptoms_present
         
         symptoms_lengths = {item: len(symptoms) for item, symptoms in node_tree_symptom_mapping.items()}
-        item_with_longest_symptoms = max(symptoms_lengths, key=symptoms_lengths.get)
-        longest_symptoms_present = node_tree_symptom_mapping[item_with_longest_symptoms]
+        if len(symptoms_lengths) > 0:
+            item_with_longest_symptoms = max(symptoms_lengths, key=symptoms_lengths.get)
+            longest_symptoms_present = node_tree_symptom_mapping[item_with_longest_symptoms]
+            checkMoreFeatures(item_with_longest_symptoms[0], longest_symptoms_present, item_with_longest_symptoms[1])
+        else:
+            print("Sorry! We don't have much information on that symptom.")
 
-        checkMoreFeatures(item_with_longest_symptoms[0], longest_symptoms_present, item_with_longest_symptoms[1])
         
     for index, symptom in enumerate(x):
         symptoms_dict[symptom] = index
@@ -284,7 +295,7 @@ def main():
     getSeverityDict()
     getDescription()
     getprecautionDict() 
-    #getInfo()
+    getInfo()
     tree_to_code()
 
 
